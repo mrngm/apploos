e589581f6d08cc24c62f4437ff05eecfd3aa02e2
Gerdriaan Mulder <hello@moeilijklastig.nl>
2024-07-06 19:31:51 +0200

    Add cmd/collector with a few basic flags

---
+++ cmd/collector/fetch.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var (
+	SupportedProtocols = []string{
+		"http://", "https://", "file://",
+	}
+)
+
+// IsSupportedSource returns the protocol and nil error if the given src is supported, or an appropriate message in
+// error otherwise.
+func IsSupportedSource(src string) (string, error) {
+	if !strings.Contains(src, "://") {
+		return "", fmt.Errorf("no protocol found, missing :// in %q", src)
+	}
+
+	for _, proto := range SupportedProtocols {
+		if strings.HasPrefix(src, proto) {
+			return proto, nil
+		}
+	}
+
+	return "", fmt.Errorf("unsupported source protocol for value %q", src)
+}
+
+func FetchSource(src string) ([]byte, error) {
+	protocol, err := IsSupportedSource(src)
+	if err != nil {
+		return nil, err
+	}
+
+	switch protocol {
+	case "http://", "https://":
+
+		return nil, fmt.Errorf("unimplemented")
+	case "file://":
+
+		return nil, fmt.Errorf("unimplemented")
+	}
+
+	return nil, fmt.Errorf("protocol %q seemed supported, but implementation is missing", protocol)
+}
--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -1,5 +1,34 @@
+// package main in cmd/collector fetches information from external sources and saves the ingested data. It may
+// format/lint the data such that it can efficiently tell through a checksum if the data was changed compared to an
+// earlier fetch.
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
+var (
+	once = flag.Bool("once", false, "If given, fetch source once, write to storage, and exit. Otherwise, keep running and fetch every interval.")
+	refreshInterval = flag.Duration("interval", time.Duration(5 * time.Minute), "Refresh source every duration with jitter. Ignored when -once is given")
+	refreshJitter = flag.Duration("jitter", time.Duration(23 * time.Second), "Apply jitter up to (-)duration on refresh interval, e.g. 5m (interval) +/- 23s (jitter)")
+	source  = flag.String("source", "", fmt.Sprintf("Fetch this source, prefixed with protocol://. Supported: %+q", SupportedProtocols))
+	saveDir = flag.String("storage", "", "Store results in this directory. If not supplied, a temporary directory will be created. If the supplied directory doesn't exist, it's created given enough permissions. Existing files in the supplied directory are never overwritten.")
+)
+
 func main() {
+	flag.Parse()
+	if flag.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		return
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctx.Done()
 
 }
