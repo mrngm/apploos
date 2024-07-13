b8c3d145b9f0f2514a8bbb9103a1cbedda4fb8e6
Gerdriaan Mulder <hello@moeilijklastig.nl>
2024-07-13 12:47:04 +0200

    processor: set a better title

--- cmd/processor/schedule.go
+++ cmd/processor/schedule.go
@@ -144,7 +144,7 @@ var htmlPrefix = `<!DOCTYPE html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width" />
-    <title>${LOCATION} - Vierdaagsefeesten ${YEAR}</title>
+    <title>Vierdaagsefeesten 2024</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
   </head>
   <body>
